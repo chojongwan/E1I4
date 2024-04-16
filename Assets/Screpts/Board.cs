1bda3794aac3109aea1963617199b6b023119a27
ACEDIA2567 <sdman2567@gmail.com>
2024-04-16 12:02:20 +0900

    난이도에 따른 카드 개수 및 위치 변경
    이재형

--- Assets/Screpts/Board.cs
+++ Assets/Screpts/Board.cs
@@ -9,20 +9,41 @@ public class Board : MonoBehaviour
     {
         Debug.Log("보드");
         //Time.timeScale = 0f;
-        StartCoroutine(SpawnCardsWithDelay());
+        StartCoroutine("SpawnCardsWithDelay", PlayerPrefs.GetInt("StageCard"));
     }
 
-    IEnumerator SpawnCardsWithDelay()
+    IEnumerator SpawnCardsWithDelay(int cardCount)
     {
-        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
-        arr = arr.OrderBy(x => Random.Range(0f, 7f)).ToArray(); // Linq를 사용한 곳
-
-        for (int i = 0; i < 16; i++)
+        // int형 List arr를 선언 후 cardCount만큼 List에 저장
+        List<int> arr = new List<int>();
+        for (int i = 0; i < cardCount; i++)
+        {
+            arr.Add(i);
+            arr.Add(i);
+        }
+        // arr의 값을 랜덤으로 섞어서 배치
+        arr = arr.OrderBy(x => Random.Range(0f, cardCount - 1)).ToList(); //Linq를 사용한곳
+        
+        for (int i = 0; i < arr.Count; i++)
         {
             GameObject go = Instantiate(card, this.transform);
 
             float x = (i % 4) * 1.4f - 2.1f;
-            float y = (i / 4) * 1.4f - 3.0f;
+            float y;
+
+            // 난이도(카드 수)에 따라서 생성되는 카드 위치 변경
+            if (cardCount == 6)
+            {
+                y = (i / 4) * 1.4f - 1.8f;
+            }
+            else if (cardCount == 8)
+            {
+                y = (i / 4) * 1.4f - 3.0f;
+            }
+            else
+            {
+                y = (i / 4) * 1.4f - 3.8f;
+            }
 
             go.transform.position = new Vector2(x, y);
             go.GetComponent<Card>().Setting(arr[i]);
@@ -30,7 +51,7 @@ public class Board : MonoBehaviour
             yield return new WaitForSeconds(0.2f); // 2초의 딜레이
         }
 
-        Gamemanager.instance.CardCount = arr.Length;
+        Gamemanager.instance.CardCount = arr.Count;
         //Time.timeScale = 1f;
         
     }
