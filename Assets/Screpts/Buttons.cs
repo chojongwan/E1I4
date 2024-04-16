4b13a5bc825b7cccf2d3627e3109c46cc17915dc
ACEDIA2567 <sdman2567@gmail.com>
2024-04-16 11:53:16 +0900

    버튼 스크립트, 버튼 기능 변경 후 스테이지 선택 버튼 추가
    이재형

---
+++ Assets/Screpts/Buttons.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public GameObject StageImg;
+
+    public void StageSelect()
+    {
+        StageImg.SetActive(true);
+    }
+
+    // 게임 재시작, 시작할 때 씬 이동
+    public void GameStart()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    // 1스테이지
+    public void Stage1()
+    {
+        // StageCard의 int형 변수에 6을 저장
+        PlayerPrefs.SetInt("StageCard", 6);
+        GameStart();
+    }
+
+    // 2스테이지
+    public void Stage2()
+    {
+        PlayerPrefs.SetInt("StageCard", 8);
+        GameStart();
+    }
+
+    // 3스테이지
+    public void Stage3()
+    {
+        PlayerPrefs.SetInt("StageCard", 10);
+        GameStart();
+    }
+}
